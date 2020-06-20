37459f0fb392aef8b0b46190f6dd393fcc16e514
rahul247 <rahul247jain@gmail.com>
2020-06-21 00:00:36 +0530

    Added all work fresh again

---
+++ orders/models.py
@@ -0,0 +1,41 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+from shop.models import Product
+# Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user', null=True)
+    first_name=models.CharField( max_length=50)
+    last_name=models.CharField( max_length=50)
+    email=models.EmailField()
+    address=models.CharField( max_length=250)
+    postal_code=models.CharField( max_length=20)
+    city=models.CharField(max_length=50)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField( auto_now =True)
+    paid=models.BooleanField(default=False)
+    
+    
+    class Meta:
+        ordering=('-created',)
+
+    def __str__(self):
+        return 'Order {}'.format(self.id)
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.item.all())
+
+class OrderItem(models.Model):
+    order=models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product=models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
+    price=models.DecimalField( max_digits=10, decimal_places=2)
+    quantity=models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return '{}'.format(self.id)
+
+    def get_cost(self):
+        return self.price*self.quantity
+    
